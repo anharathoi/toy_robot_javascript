93d0336196ea05d0f9e5cbdd8f43d9c8f15a089b
anharathoit <anharzihan@gmail.com>
2019-07-26 12:43:13 -0700

    added test and method for place, move and new instance)

--- Robot/robot.js
+++ Robot/robot.js
@@ -1,5 +1,6 @@
 class Robot {
-  constructor(x, y, direction){
+
+  place(x, y, direction){
     this.x = x
     this.y = y
     this.direction = direction
@@ -9,16 +10,16 @@ class Robot {
     switch(this.direction) {
       case "NORTH":
           this.y += 1
-        break;
+          return this
       case "SOUTH":
           this.y -= 1
-        break;
+          return this
       case "EAST":
         this.x += 1
-        break;
+        return this
       case "WEST":
         this.x -= 1
-        break;
+        return this
       default:
         return  "INVALID INPUT"
     }
@@ -31,9 +32,45 @@ class Robot {
       direction: this.direction
     }
   }
+
+  turnLeft(){
+    switch(this.direction) {
+      case "NORTH":
+          this.direction = "WEST"
+        break;
+      case "SOUTH":
+          this.direction = "EAST"
+        break;
+      case "EAST":
+          this.direction = "NORTH"
+        break;
+      case "WEST":
+          this.direction = "SOUTH"
+        break;
+      default:
+        return  "INVALID INPUT"
+    }
+  }
+
+  turnRight(){
+    switch(this.direction) {
+      case "NORTH":
+          this.direction = "EAST"
+        break;
+      case "SOUTH":
+          this.direction = "WEST"
+        break;
+      case "EAST":
+          this.direction = "SOUTH"
+        break;
+      case "WEST":
+          this.direction = "NORTH"
+        break;
+      default:
+        return  "INVALID INPUT"
+    }
+  }
 }
-const robot = new Robot(0, 0, "NORTH")
-console.log(robot)
 
 module.exports = {
   Robot
--- Tests/robot.test.js
+++ Tests/robot.test.js
@@ -1,33 +1,102 @@
 const { Robot }  = require('../Robot/robot')
 
-describe("while facing north", () => {
-  const robot = new Robot(0, 0, "NORTH")
-  test('moving north', () => {
-    robot.move()
-    expect(robot.y).toBe(1)
+// robot instance
+describe("new Robot", () => {
+  const robot = new Robot
+
+  test("is instance of Robot", () => {
+    expect(robot).toBeInstanceOf(Robot)
   })
 })
 
-describe("while facing south", () => {
-  const robot = new Robot(0, 0, "SOUTH")
-  test('moving south', () => {
-    robot.move()
-    expect(robot.y).toBe(-1)
+// place method
+describe("placing robot on the board", () => {
+  let robot = new Robot
+  robot.place(0, 0, "SOUTH")
+  test("robot has an x position", () => {
+    expect(robot.x).toBe(0)
+  })
+
+  test("robot has an y position", () => {
+    expect(robot.y).toBe(0)
+  })
+
+  test("robot has a direction property", () => {
+    expect(robot.direction).toBe("SOUTH")
   })
 })
 
-describe("while facing east", () => {
-  const robot = new Robot(0, 0, "EAST")
-  test('moving east', () => {
-    robot.move()
-    expect(robot.x).toBe(1)
+// move method
+describe(".move()", () => {
+  describe("while facing north", () => {
+    let robot
+    beforeEach( () => {
+      robot = new Robot
+      robot.place(0, 0, "NORTH")
+    })
+    test('moves north one space', () => {
+      robot.move()
+      expect(robot.y).toBe(1)
+    })
+
+    test('moves north by 4 spaces', () => {
+      robot.move().move().move().move()
+      expect(robot.y).toBe(4)
+    })
+  })
+
+  describe("while facing south", () => {
+    let robot
+    beforeEach( () => {
+      robot = new Robot
+      robot.place(0, 0, "SOUTH")
+    })
+  
+    test('moves south one space', () => {
+      robot.move()
+      expect(robot.y).toBe(-1)
+    })
+
+    test('moves south by 4 spaces', () => {
+      robot.move().move().move().move()
+      expect(robot.y).toBe(-4)
+    })
+  })
+
+  describe("while facing east", () => {
+    let robot
+    beforeEach( () => {
+      robot = new Robot
+      robot.place(0, 0, "EAST")
+    })
+    
+    test("moves east one space", () => {
+      robot.move()
+      expect(robot.x).toBe(1)
+    })
+
+    test("moves east four spaces", () => {
+      robot.move().move().move().move()
+      expect(robot.x).toBe(4)
+    })
+  })
+
+  describe("while facing west", () => {
+    let robot
+    beforeEach( () => {
+      robot = new Robot
+      robot.place(0, 0, "WEST")
+    })
+
+    test('moves west one space', () => {
+      robot.move()
+      expect(robot.x).toBe(-1)
+    })
+    
+    test('moves west four spaces', () => {
+      robot.move().move().move().move()
+      expect(robot.x).toBe(-4)
+    })
   })
 })
 
-describe("while facing west", () => {
-  const robot = new Robot(0, 0, "WEST")
-  test('moving west', () => {
-    robot.move()
-    expect(robot.x).toBe(-1)
-  })
-})
